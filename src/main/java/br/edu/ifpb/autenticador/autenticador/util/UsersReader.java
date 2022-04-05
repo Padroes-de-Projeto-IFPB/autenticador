f4bea8f69d3a83a605d4a5278e7506665cf15770
Diego Figueiredo <71049855+diegofigmedeiros@users.noreply.github.com>
2022-04-04 22:09:31 -0300

    feat: Singleton

--- src/main/java/br/edu/ifpb/autenticador/autenticador/util/UsersReader.java
+++ src/main/java/br/edu/ifpb/autenticador/autenticador/util/UsersReader.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -13,14 +14,36 @@ import static java.util.Arrays.asList;
 // TODO - Transformar essa classe em um SINGLETON para guardar lista de usuários do arquivo, ao invés de carregar do arquivo em cada chamada
 public class UsersReader {
 
+    private static volatile UsersReader instance;
+    private static final String JSON_FILE = "users.json";
+    private static List<User> user        = new ArrayList<>();
+
     private UsersReader() {}
 
-    private static final String JSON_FILE = "users.json";
+    public static UsersReader getInstance() throws URISyntaxException, IOException {
+        UsersReader result = instance;
+        if (result != null) {
+            return result;
+        }
 
-    public static List<User> loadUsersFromJson() throws URISyntaxException, IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        User[] users = objectMapper.readValue(new File(ClassLoader.getSystemResource(JSON_FILE).toURI()), User[].class);
-        return asList(users);
+        synchronized (UsersReader.class) {
+            if (instance == null) {
+                instance = new UsersReader();
+            }
+
+            loadUsersFromJson();
+            return instance;
+        }
     }
 
+    public static void loadUsersFromJson() throws URISyntaxException, IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        User[] users = objectMapper.readValue(new File(ClassLoader.getSystemResource(JSON_FILE).toURI())
+                , User[].class);
+        user = asList(users);
+    }
+
+    public static List<User> getUser() throws URISyntaxException, IOException {
+        return user;
+    }
 }
