0355574d0adb6c1e157b4cf02241f278c673d89d
João Jorge <52219312+JoaoJorgeEF@users.noreply.github.com>
2022-04-05 19:43:44 -0300

    alteração para o Singleton

--- src/main/java/br/edu/ifpb/autenticador/autenticador/util/UsersReader.java
+++ src/main/java/br/edu/ifpb/autenticador/autenticador/util/UsersReader.java
@@ -13,14 +13,34 @@ import static java.util.Arrays.asList;
 // TODO - Transformar essa classe em um SINGLETON para guardar lista de usuários do arquivo, ao invés de carregar do arquivo em cada chamada
 public class UsersReader {
 
-    private UsersReader() {}
+    private static UsersReader instance;
+
+    private User[] users;
+
+    private UsersReader() throws URISyntaxException, IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        this.users = objectMapper.readValue(new File(ClassLoader.getSystemResource(JSON_FILE).toURI()), User[].class);
+
+    }
 
     private static final String JSON_FILE = "users.json";
 
+    public static UsersReader getInstance() throws URISyntaxException, IOException {
+        UsersReader teste = instance;
+        if (teste != null) {
+            return teste;
+        }
+        synchronized(UsersReader.class) {
+            if (instance == null) {
+                instance = new UsersReader();
+            }
+            return instance;
+        }
+    }
+
+
     public static List<User> loadUsersFromJson() throws URISyntaxException, IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        User[] users = objectMapper.readValue(new File(ClassLoader.getSystemResource(JSON_FILE).toURI()), User[].class);
-        return asList(users);
+        return asList(UsersReader.getInstance().users);
     }
 
 }
